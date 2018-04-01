3f2b2b047211a7cabdcc7b3a622a85fc047e4ded
Davia DeNisco <davia622@gmail.com>
2018-04-01 15:32:24 -0400

    Update app.js, knexfile.js, and populate seeds.js, add db.js

---
+++ db.js
@@ -0,0 +1,5 @@
+const devConfig = require("./knexfile")[process.env.NODE_ENV || 'development'];
+
+const knex = require('knex')(devConfig);
+
+module.exports = knex;
--- knexfile.js
+++ knexfile.js
@@ -2,5 +2,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost:5432/studentinfodb'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 }
