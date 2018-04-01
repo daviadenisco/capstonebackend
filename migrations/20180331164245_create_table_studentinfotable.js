f4ba6a293f622bb2b6e8d1ed6ad54f4a9f4ebc1c
Davia DeNisco <davia622@gmail.com>
2018-03-31 21:51:00 -0400

    create migrations, update but not run seeds, update users, update knexfile.js

---
+++ migrations/20180331164245_create_table_studentinfotable.js
@@ -0,0 +1,51 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("studentinfotable", (table) => {
+    // id
+    table.increments();
+    // headshot
+    table.string('headshot');
+    // full name
+    table.string('fullname');
+    // github
+    table.string('github');
+    // linkedin
+    table.string('linkedin');
+    // email address
+    table.string('email');
+    // personal website
+    table.string('personalsite');
+    // linke to deployed capstone project
+    table.string('capstonelink');
+    // blurb describing the capstone project
+    table.string('capstonedesc');
+    // q1 project
+    table.string('q1project');
+    // q2 project
+    table.string('q2project');
+    // q3 project
+    table.string('q3project');
+    // brief bio
+    table.string('bio');
+    // fun fact
+    table.string('funfact');
+    // fun pic
+    table.string('funpic');
+    // request resume
+    table.string('requestresume');
+    // extra space 1
+    table.string('extraspace1');
+    // extra space 2
+    table.string('extraspace2');
+    // extra space 3
+    table.string('extraspace3');
+    // extra space 4
+    table.string('extraspace4');
+    // extra space 5
+    table.string('extraspace5');
+    // timestamps
+    table.timestamps(true, true);
+  });
+};
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable("studentinfotable");
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,51 @@
 var express = require('express');
 var router = express.Router();
+const knex = require('../db');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('The slash route is working!');
+});
+
+router.get('/studentinfotable', (req, res) => {
+  // this returns an array of objects representing all the rows in the table and all the columns in the rows
+  knex('studentinfotable').then(rows => res.json(rows));
+})
+
+router.get('/studentinfotable/:id', (req, res) => {
+  // access the request object, tack on parameters and then the id
+  const studentinfotableid = req.params.id;
+  knex('studentinfotable').where('id', studentinfotableid) // SELECT * from studentinfotable WHERE id=studentinfotableid
+  .then(rows => {
+    // we want the first thing in the array, which is the id, aka rows[0]
+    const foundstudentinfotableid = rows[0];
+    // send the res as json
+    res.json(foundstudentinfotableid);
+  })
+})
+
+router.patch('/studentinfotable/:id', (req, res) => {
+  // headshot, fullname, github, linkedin, email, personalsite, capstonelink, capstonedesc, q1project, q2project, q3project, bio, funfact, funpic, requestresume, extraspace1, extraspace2, extraspace3, extraspace4, extraspace5
+  const studentinfotableid = req.params.id;
+  // debug with console.log(`req.body: ${res.body}`);
+  const {headshot, fullname, github, linkedin, email, personalsite, capstonelink, capstonedesc, q1project, q2project, q3project, bio, funfact, funpic, requestresume, extraspace1, extraspace2, extraspace3, extraspace4, extraspace5} = req.body;
+
+  knex('studentinfotable')
+  .where('id', foundstudentinfotableid)
+  .returning('*')
+  .update({headshot, fullname, github, linkedin, email, personalsite, capstonelink, capstonedesc, q1project, q2project, q3project, bio, funfact, funpic, requestresume, extraspace1, extraspace2, extraspace3, extraspace4, extraspace5})
+  .then (rows => {
+    const studentid = rows[0];
+    res.json(studentid)
+  });
+});
+
+router.delete('/studentinfotable/:id', (req, res) => {
+  const studentid = req.params.id;
+  knex('studentinfotable')
+  .where('id', studentid)
+  .del()
+  .then(res.send('Deleted'));
 });
 
 module.exports = router;
